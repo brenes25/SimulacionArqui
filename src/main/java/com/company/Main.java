f84469d9c7a542c925290a677d698a9253864f20
Silvia <silbrenes63@gmail.com>
2018-06-13 14:29:51 -0600

    adding maven.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Processor processor = new Processor();
-        DataParser dataParser= new DataParser(processor);
+        DataParser dataParser = new DataParser(processor);
         dataParser.parseFile("./src/resources/0.txt");
         processor.printInstructionMemory();
         Context context = (Context) processor.getContextQueue().poll();
